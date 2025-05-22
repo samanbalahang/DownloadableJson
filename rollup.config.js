4a24d09371bb4ccd9b0f5adacb27ec5f3a2f34bd
saman balahang <saman.balahang.2012@gmail.com>
2025-05-23 01:19:01 +0330

    start03

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'dist/index.umd.js',
       format: 'umd',
-      name: 'FileDownloadUtil'
+      name: 'JSONExporter' // Global variable name for browser
     },
     {
       file: 'dist/index.esm.js',
       format: 'es'
+    },
+    {
+      file: 'dist/index.js',
+      format: 'cjs'
     }
   ],
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    terser()
-  ]
+  plugins: [nodeResolve(), commonjs(), terser()]
 };
